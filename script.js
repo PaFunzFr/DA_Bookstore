1f35ce8b2d9aed8e2a79a7151141d10b4e846289
funZ <153361103+PaFunzFr@users.noreply.github.com>
2025-01-14 17:53:12 +0100

    moving files

--- script.js
+++ script.js
@@ -4,7 +4,7 @@ function renderInit() {
   loadDataBase();
   renderAllBookCards();
   renderFooter();
-  checkUserLogin()
+  checkUserLogin();
 }
 
 // initialize json data (likes & comments) to localStorage
@@ -12,15 +12,18 @@ function renderInit() {
 function loadDataBase() {
   for (i = 0; i < books.length; i++) {
     let likeObject = {
-      likes : books[i].likes,
-      liked : books[i].liked,
-      heartIcon : "./assets/img/03_icons/heart-empty.png"
-    }
+      likes: books[i].likes,
+      liked: books[i].liked,
+      heartIcon: "./assets/img/03_icons/heart-empty.png",
+    };
     if (!localStorage.getItem(`bookLikes${i}`)) {
-    localStorage.setItem(`bookLikes${i}`, JSON.stringify(likeObject))
+      localStorage.setItem(`bookLikes${i}`, JSON.stringify(likeObject));
     }
     if (!localStorage.getItem(`bookComments${i}`)) {
-      localStorage.setItem(`bookComments${i}`, JSON.stringify(books[i].comments));
+      localStorage.setItem(
+        `bookComments${i}`,
+        JSON.stringify(books[i].comments)
+      );
     }
   }
 }
@@ -29,9 +32,9 @@ function loadDataBase() {
 function showAllComments(bookKeyIndex) {
   let bookKey = `bookComments${bookKeyIndex}`;
   let bookComments = JSON.parse(localStorage.getItem(bookKey)) || [];
-  let commentsHTML = ''; 
+  let commentsHTML = "";
   for (let index = bookComments.length - 1; index >= 0; index--) {
-      commentsHTML += `
+    commentsHTML += `
           <tr>
               <td class="comment-author">${bookComments[index].name}:</td>
               <td class="comment-text">${bookComments[index].comment}</td>
@@ -44,11 +47,11 @@ function postComment(index) {
   let commentInput = document.getElementById(`commentField${index}`).value;
   let newComment = {
     name: JSON.parse(localStorage.getItem("user")),
-    comment: commentInput
+    comment: commentInput,
   };
   updateComments(index, newComment);
-  document.getElementById(`commentField${index}`).value = '';
-  renderInit()
+  document.getElementById(`commentField${index}`).value = "";
+  renderInit();
 }
 
 function updateComments(index, object) {
@@ -59,42 +62,42 @@ function updateComments(index, object) {
 }
 
 function loginUser() {
-  let userName = document.getElementById('loginName').value;
+  let userName = document.getElementById("loginName").value;
   if (userName != "") {
-  localStorage.setItem("user", JSON.stringify(userName));
-  closeLoginMenu();
-  renderInit()
+    localStorage.setItem("user", JSON.stringify(userName));
+    closeLoginMenu();
+    renderInit();
   }
 }
 
 function renderAllBookCards() {
-  document.getElementById('bookContent').innerHTML = ``; 
+  document.getElementById("bookContent").innerHTML = ``;
   for (let i = 0; i < books.length; i++) {
     renderBookCard(i);
   }
-};
+}
 
 function emptyValue(event) {
-    event.target.value = '';
+  event.target.value = "";
 }
 
 function showFullHeart(event) {
-  event.target.src = "./assets/img/03_icons/heart-full.png"
+  event.target.src = "./assets/img/03_icons/heart-full.png";
 }
 
 function showEmptyHeart(event) {
-    event.target.src = "./assets/img/03_icons/heart-empty.png"
+  event.target.src = "./assets/img/03_icons/heart-empty.png";
 }
 
 function clickHeartIcon(event, index) {
   let likesData = JSON.parse(localStorage.getItem(`bookLikes${index}`));
   if (!likesData.liked) {
-    makeFavorite(index , likesData);
+    makeFavorite(index, likesData);
     popColorScale(event);
   } else {
-    noFavorite(index, likesData)
+    noFavorite(index, likesData);
     popColorScale(event);
-    setTimeout(function() {
+    setTimeout(function () {
       showEmptyHeart(event);
     }, 200);
   }
@@ -103,8 +106,8 @@ function clickHeartIcon(event, index) {
 function makeFavorite(index, likesData) {
   isHeartClicked = true;
   let currentLikes = likesData.likes;
-  currentLikes ++
-  let likeObject = likeObjectTemplate(currentLikes, true, "full")
+  currentLikes++;
+  let likeObject = likeObjectTemplate(currentLikes, true, "full");
   localStorage.setItem(`bookLikes${index}`, JSON.stringify(likeObject));
   renderAllBookCards(index);
 }
@@ -112,17 +115,19 @@ function makeFavorite(index, likesData) {
 function noFavorite(index, likesData) {
   isHeartClicked = false;
   let currentLikes = likesData.likes;
-  currentLikes --
-  let likeObject = likeObjectTemplate(currentLikes, false, "empty")
+  currentLikes--;
+  let likeObject = likeObjectTemplate(currentLikes, false, "empty");
   localStorage.setItem(`bookLikes${index}`, JSON.stringify(likeObject));
-  showFavorites()
+  showFavorites();
 }
 
 function popColorScale(event) {
-  event.target.style.filter = "invert(10%) sepia(45%) saturate(6620%) hue-rotate(339deg) brightness(200%) contrast(101%)";
+  event.target.style.filter =
+    "invert(10%) sepia(45%) saturate(6620%) hue-rotate(339deg) brightness(200%) contrast(101%)";
   event.target.style.transform = "scale(1.2)";
-  setTimeout(function() {
-    event.target.style.filter = "invert(10%) sepia(45%) saturate(6620%) hue-rotate(339deg) brightness(99%) contrast(101%)";
+  setTimeout(function () {
+    event.target.style.filter =
+      "invert(10%) sepia(45%) saturate(6620%) hue-rotate(339deg) brightness(99%) contrast(101%)";
     event.target.style.transform = "scale(1)";
   }, 200);
 }
@@ -151,27 +156,27 @@ function hideElementsHeader() {
   }
 }
 function closeLoginMenu() {
-  document.getElementById('loginSection').style.display = "none";
-  document.getElementById('mainSection').style.display = "flex";
+  document.getElementById("loginSection").style.display = "none";
+  document.getElementById("mainSection").style.display = "flex";
 }
 
 function showLoginMenu() {
-  document.getElementById('loginSection').style.display = "flex";
-  document.getElementById('mainSection').style.display = "none";
+  document.getElementById("loginSection").style.display = "flex";
+  document.getElementById("mainSection").style.display = "none";
 }
 
 function userLogout() {
   localStorage.removeItem("user");
   resetFavorites();
-  showLoginMenu()
-  renderInit()
+  showLoginMenu();
+  renderInit();
 }
 
 function resetFavorites() {
-  for (i = 0; i < books.length; i ++) {
+  for (i = 0; i < books.length; i++) {
     let likesData = JSON.parse(localStorage.getItem(`bookLikes${i}`));
     let currentLikes = likesData.likes;
-    let likeObject = likeObjectTemplate(currentLikes, false, "empty")
+    let likeObject = likeObjectTemplate(currentLikes, false, "empty");
     localStorage.setItem(`bookLikes${i}`, JSON.stringify(likeObject));
     renderAllBookCards(i);
   }
@@ -179,10 +184,10 @@ function resetFavorites() {
 
 function likeObjectTemplate(likeCount, likeStatus, heartStatus) {
   return {
-    likes : likeCount,
-    liked : likeStatus,
-    heartIcon : `./assets/img/03_icons/heart-${heartStatus}.png`
-  }
+    likes: likeCount,
+    liked: likeStatus,
+    heartIcon: `./assets/img/03_icons/heart-${heartStatus}.png`,
+  };
 }
 
 function showFavorites() {
@@ -190,6 +195,6 @@ function showFavorites() {
     let likesData = JSON.parse(localStorage.getItem(`bookLikes${i}`));
     if (!likesData.liked) {
       document.getElementById(`bookCard${i}`).style.display = "none";
-    } 
+    }
   }
 }
